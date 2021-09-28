fbad8151f57ad332fb225e9a9a26189ff503766d
Riya20395 <90259775+Riya20395@users.noreply.github.com>
2021-09-28 17:38:25 -0400

    activity 1-3 are completed 09/28/2021

--- JavaHandsOn/src/main/java/com/example/activity/three/FunWithStrings.java
+++ JavaHandsOn/src/main/java/com/example/activity/three/FunWithStrings.java
@@ -8,34 +8,59 @@ public class FunWithStrings {
 		//Using the .length() method and .charAt() method
 		//Loop through the input text and return the first instance of a vowel (aeiou)
 		//Should return -1 if no vowels present
-		
+		char[] c= {'a','e','i','o','u'};
+		char ans = 0;
+		for(int i=0;i<input.length();i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(input.charAt(i)==c[j])
+				{
+					ans=c[j];
+				}
+			}
+			
+		}
 		//This should be modified to return the correct char
-		return '0';
+		return ans;
 	}
 	
 	
 	public String replaceAllSpaces(String input) {
 		//Using the .replaceAll() method, replace all the spaces in the input text with '-'
 		
-		
+		String str=input.replaceAll("\s", "-");
+	
 		//This should be modified to return the correct string
-		return "";
+		return str;
 	}
 	
 	
 	public boolean containsWord(String input, String word) {
 		//Expect the input to be crazy like this "InPUT iS noT UNIform"
 		//Use the string methods .toLowerCase() and .contains to find out if the given word is contained inside of input
-		
+	
+		boolean compare=input.toLowerCase().contains(word);
+				
 		//This should be modified to return the correct boolean
-		return false;
+		return compare;
 	}
 	
 	public boolean isPalindrome(String input) {
 		//Use your knowledge of loops, and .charAt() to find out if the input is a palendrome
-		
+		int length = input.length();
+		 String s="";
+		 boolean b;
+	      for ( int i = length - 1; i >= 0; i-- )
+	         s = s + input.charAt(i);
+	 
+	      if (input.equals(s))
+	         b=true;
+	      else
+	         b=false;
+	 
 		//This should be modified to return the correct boolean
-		return false;
+		return b;
 	}
 	
 }
--- JavaHandsOn/src/main/java/com/example/activity/two/ControlFlowActivity.java
+++ JavaHandsOn/src/main/java/com/example/activity/two/ControlFlowActivity.java
@@ -8,25 +8,48 @@ public class ControlFlowActivity {
 		//The method should return bar if the input is divisible by 5
 		//The method should return buz if the input is divisible by both 3 and 5
 		//HINT use the modulos operator to determine the divisibility of the input
-		
-		
-		return "This should return the correct output";
+		String s;
+		if(input%3==0) 
+			s="foo";
+		else if(input%5==0)
+			s="bar";
+		else if(input%15==0)
+			s="buz";
+		else 
+			s="";
+		return s;
 	}
 	
+	
+
 	public int sumOfEvens() {
 		//Fill out the method below that adds up all of the even numbers from 1 to 100
 		//Use a for loop to achieve this
+		int j=0;
+		for(int i=0;i<=100;i++)
+		{
+			if(i%2==0)
+			{
+				
+				j+=i;
+			}
+		}
 		
 		//This should be changed to return the correct output
-		return 0;
+		return j;
 	}
 	
 	public int sumOfNumbers(int input) {
 		//Fill out the method below that adds up all the numbers from 1 to the given input using a while loop
-
+		int j=0;
+		for(int i=0;i<=input;i++)
+		{
+				
+				j+=i;
+		}
 		
 		//This should be changed to return the correct output
-		return 0;
+		return j;
 	}
 
 }
