31cb314903e10337f2a562265317ece1ad0cdf80
Virut1316 <54875369+Virut1316@users.noreply.github.com>
2021-09-28 19:40:23 -0400

    1 to 4 Exc
    1 to 4 Exc

--- JavaHandsOn/src/main/java/com/example/activity/four/FunWithArrays.java
+++ JavaHandsOn/src/main/java/com/example/activity/four/FunWithArrays.java
@@ -5,23 +5,42 @@ public class FunWithArrays {
 	public int[] squares(int a, int b, int c, int d) {
 		//Given the four inputs, square them, and put them in an array to return
 		
+		int[] result = {a*a,b*b,c*c,d*d};
 		
 		//This should be modified to return the correct array
-		return new int[4];
+		return result;
 	}
 	
 	public int arraySum(int[] nums) {
 		//Return the sum of the numbers inside of the given array
+		int total=0;
+		
+		for (int i : nums) {
+			total+=i;
+		}
 		
 		//This should be modifed to return the correct number
-		return 0;
+		return total;
+	}
+	
+	public double arraySumDouble(double[] nums) {
+		//Return the sum of the numbers inside of the given array
+		double total=0;
+		
+		for (double i : nums) {
+			total+=i;
+		}
+		
+		//This should be modifed to return the correct number
+		return total;
 	}
 	
 	public double arrayAverage(double[] nums) {
 		//Return the average of the numbers given in the array
 		
+		
 		//This should be modified to return the correct number
-		return 0.0;
+		return arraySumDouble(nums)/nums.length;
 	}
 	
 }
