0ffaa59f9ac0e7303e5c12ce6bf14f88ad1c7f33
CharlesSmith98 <charles.w.smith@outlook.com>
2021-09-29 08:27:03 -0400

    Completed Activity 4

--- JavaHandsOn/src/main/java/com/example/activity/four/FunWithArrays.java
+++ JavaHandsOn/src/main/java/com/example/activity/four/FunWithArrays.java
@@ -4,24 +4,36 @@ public class FunWithArrays {
 	
 	public int[] squares(int a, int b, int c, int d) {
 		//Given the four inputs, square them, and put them in an array to return
-		
+		int[] nums = {a, b, c, d};
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = nums[i] * nums[i];
+		}
 		
 		//This should be modified to return the correct array
-		return new int[4];
+		return nums;
 	}
 	
 	public int arraySum(int[] nums) {
 		//Return the sum of the numbers inside of the given array
+		int sum = 0;
+		for (int i: nums) {
+			sum += i;
+		}
 		
 		//This should be modifed to return the correct number
-		return 0;
+		return sum;
 	}
 	
 	public double arrayAverage(double[] nums) {
 		//Return the average of the numbers given in the array
+		double average = 0.0;
+		for (double n : nums) {
+			average += n;
+		}
+		average /= nums.length;
 		
 		//This should be modified to return the correct number
-		return 0.0;
+		return average;
 	}
 	
 }
