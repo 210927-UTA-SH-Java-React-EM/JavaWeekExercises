ce6bf3bdd6a60adada7a308b247f3cead3180066
edwinesca1 <edco_er@hotmail.com>
2021-09-29 16:00:41 -0700

    exercises 3 and 4 complete

--- JavaHandsOn/src/main/java/com/example/activity/four/FunWithArrays.java
+++ JavaHandsOn/src/main/java/com/example/activity/four/FunWithArrays.java
@@ -4,24 +4,41 @@ public class FunWithArrays {
 	
 	public int[] squares(int a, int b, int c, int d) {
 		//Given the four inputs, square them, and put them in an array to return
+		int a2 = a*a;
+		int b2 = b*b;
+		int c2 = c*c;
+		int d2 = d*d;
 		
+		int squareArray[] = new int[] {a2, b2, c2, d2};
 		
 		//This should be modified to return the correct array
-		return new int[4];
+		return squareArray;
 	}
 	
 	public int arraySum(int[] nums) {
 		//Return the sum of the numbers inside of the given array
-		
+		int addVal = 0;
+		for(int i = 0; i < nums.length ; i++)
+		{
+			addVal = addVal + nums[i];
+		}
 		//This should be modifed to return the correct number
-		return 0;
+		return (addVal);
 	}
 	
 	public double arrayAverage(double[] nums) {
 		//Return the average of the numbers given in the array
+		double addVal = 0;
+		double avr = 0;
+		for(int i = 0; i < nums.length ; i++)
+		{
+			addVal = addVal + nums[i];
+		}
+		
+		avr = addVal/nums.length;
 		
 		//This should be modified to return the correct number
-		return 0.0;
+		return avr;
 	}
 	
 }
--- JavaHandsOn/src/main/java/com/example/activity/three/FunWithStrings.java
+++ JavaHandsOn/src/main/java/com/example/activity/three/FunWithStrings.java
@@ -4,13 +4,28 @@ public class FunWithStrings {
 	
 	//In this activity you will use your knowledge of control flow and string to complete the below methods
 	
-	public char getFirstVowel(String input) {
+	public int getFirstVowel(String input) {
 		//Using the .length() method and .charAt() method
 		//Loop through the input text and return the first instance of a vowel (aeiou)
 		//Should return -1 if no vowels present
+		int i = 0;
+		int j = input.length() - 1;
+		String str = input.toLowerCase();
+		int val = 0;
+		
+		while(i <= j)
+		{
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u')
+			{
+				return i;
+			}
+			
+			i++;
+		}
+		
 		
 		//This should be modified to return the correct char
-		return '0';
+		return (-1);
 	}
 	
 	
@@ -27,16 +42,36 @@ public class FunWithStrings {
 	public boolean containsWord(String input, String word) {
 		//Expect the input to be crazy like this "InPUT iS noT UNIform"
 		//Use the string methods .toLowerCase() and .contains to find out if the given word is contained inside of input
+	    String str = input.toLowerCase();
+	    boolean val = false;
+		
+		if (str.contains(word) == true)
+	    {
+	    	val = true;
+	    }
 		
 		//This should be modified to return the correct boolean
-		return false;
+		return val;
 	}
 	
 	public boolean isPalindrome(String input) {
 		//Use your knowledge of loops, and .charAt() to find out if the input is a palendrome
+		int i = 0;
+		int j = input.length() - 1;
+		
+		while (i < j)
+		{
+			if (input.charAt(i) != input.charAt(j))
+			{
+				return false;
+			}
+			
+			i++;
+			j--;
+		}
 		
 		//This should be modified to return the correct boolean
-		return false;
+		return true;
 	}
 	
 }
