5f3b7f4f172676cb682b6eba4c350801ad596d3b
Taylor Jones <taylor.jones@revature.net>
2021-09-29 20:58:26 -0700

    finished activities 1, 2, 3

--- JavaHandsOn/src/main/java/com/example/activity/three/FunWithStrings.java
+++ JavaHandsOn/src/main/java/com/example/activity/three/FunWithStrings.java
@@ -4,38 +4,76 @@ public class FunWithStrings {
 	
 	//In this activity you will use your knowledge of control flow and string to complete the below methods
 	
-	public char getFirstVowel(String input) {
+	public int getFirstVowel(String input) {
 		//Using the .length() method and .charAt() method
 		//Loop through the input text and return the first instance of a vowel (aeiou)
 		//Should return -1 if no vowels present
-		
+		int lengthOfInput;
+		StringBuilder sb = new StringBuilder();
+		int vowelAt = 0;
+		lengthOfInput = input.length();
+		for(int i = 0; i<lengthOfInput;i++)
+		{
+			sb.append(input.charAt(i));
+			if(input.charAt(i) == 'a' ||input.charAt(i) == 'e' ||input.charAt(i) == 'i'||input.charAt(i) == 'o' ||input.charAt(i) == 'u')
+			{
+				vowelAt = sb.length() - 1;
+				return vowelAt;
+			}
+		}
+		return -1;
 		//This should be modified to return the correct char
-		return '0';
 	}
 	
 	
 	public String replaceAllSpaces(String input) {
 		//Using the .replaceAll() method, replace all the spaces in the input text with '-'
-		
-		
+		String replaced = input.replaceAll(" ", "-");
 		//This should be modified to return the correct string
-		return "";
+		return replaced;
 	}
 	
 	
 	public boolean containsWord(String input, String word) {
 		//Expect the input to be crazy like this "InPUT iS noT UNIform"
 		//Use the string methods .toLowerCase() and .contains to find out if the given word is contained inside of input
-		
+		boolean bool;
+		input = input.toLowerCase();
+		word = word.toLowerCase();
+		if(input.contains(word))
+		{
+			return true;
+		}
 		//This should be modified to return the correct boolean
 		return false;
 	}
 	
 	public boolean isPalindrome(String input) {
 		//Use your knowledge of loops, and .charAt() to find out if the input is a palendrome
-		
-		//This should be modified to return the correct boolean
-		return false;
+		int reverseString;
+		reverseString = input.length();
+		for(int i = 0; i<input.length()/2; i++)
+		{
+			//System.out.println(reverseString);
+			//System.out.println(input.charAt(i));
+			//System.out.println(input.charAt(reverseString - 1));
+			if(input.charAt(i) == input.charAt(reverseString - 1))
+			{
+				reverseString -= 1;
+			}
+			else return false;
+		}
+		return true;
 	}
 	
+
+
+public static void main(String args[])
+	{
+	FunWithStrings Test = new FunWithStrings();
+	//System.out.println(Test.getFirstVowel("fffdddefff"));
+	//System.out.println(Test.replaceAllSpaces("Four score and seven years ago"));
+	//System.out.println(Test.containsWord("FincH", "Aretha"));
+	//System.out.println(Test.isPalindrome("detartrated"));
+	}
 }
--- JavaHandsOn/src/main/java/com/example/activity/two/ControlFlowActivity.java
+++ JavaHandsOn/src/main/java/com/example/activity/two/ControlFlowActivity.java
@@ -9,24 +9,47 @@ public class ControlFlowActivity {
 		//The method should return buz if the input is divisible by both 3 and 5
 		//HINT use the modulos operator to determine the divisibility of the input
 		
-		
-		return "This should return the correct output";
+		if(input % 3 == 0 && input % 5 != 0)
+		{
+			return "foo";
+		}
+		else if (input % 5 == 0 && input % 3 != 0) {
+			return "bar";
+		}
+		else if (input % 3 == 0 && input % 5 == 0)
+		{
+			return "buz";
+		}
+		else
+		{
+			return "None of these";
+		}
 	}
 	
 	public int sumOfEvens() {
 		//Fill out the method below that adds up all of the even numbers from 1 to 100
 		//Use a for loop to achieve this
-		
+		int sum = 0;
 		//This should be changed to return the correct output
-		return 0;
+		for(int i = 2; i<=100; i = i + 2)
+		{
+		sum = sum + i;
+		}
+		return sum;
 	}
 	
 	public int sumOfNumbers(int input) {
 		//Fill out the method below that adds up all the numbers from 1 to the given input using a while loop
-
+		int additionStarting = 1;
+		int addition = 0;
+		while(additionStarting <= input)
+		{
+			addition += additionStarting;
+			additionStarting ++;
+		}
 		
 		//This should be changed to return the correct output
-		return 0;
+		return addition;
 	}
 
 }
