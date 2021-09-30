5a1db2e66d7c17f1235fd80f11f8ea831e93ed43
suzannaShevchuk <suzanna.shevchuk@gmail.com>
2021-09-30 12:24:51 -0700

    Finished activity 7

--- JavaHandsOn/src/main/java/com/example/activity/seven/InsufficientFundsException.java
+++ JavaHandsOn/src/main/java/com/example/activity/seven/InsufficientFundsException.java
@@ -1,6 +1,14 @@
 package com.example.activity.seven;
 
 //Turn this ordinary class into a unchecked exception
-public class InsufficientFundsException {
-
+public class InsufficientFundsException  extends RuntimeException {
+	
+	public InsufficientFundsException(String message) {
+		super(message);
+	}
+	
+	public InsufficientFundsException() {
+		super("Insufficient Funds");
+	}
+	
 }
--- JavaHandsOn/src/main/java/com/example/activity/seven/User.java
+++ JavaHandsOn/src/main/java/com/example/activity/seven/User.java
@@ -13,18 +13,31 @@ public class User {
 	}
 	
 	//Implement the method below to throw a custom Exception called InvalidCredentialsException if the users credentials are incorrect
-	public boolean login(String username, String password) {
-		
+	public boolean login(String username, String password) throws InvalidCredentialsException{
+		if(username==this.username && password==this.password)
+		{
+			return true;
+		}
+		else
+		{
+			throw new InvalidCredentialsException();
+		}
 		//If the user logged in successfully return true
-		return false;
 	}
 	
 	//Implement the method below to throw a custom Exception called InsufficientFundsException if the users balance is not high enough to
 	//pay for the cost
-	public int pay(int cost) {
-		
+	public int pay(int cost) throws InsufficientFundsException{
+		if(this.balance<cost)
+		{
+			throw new InsufficientFundsException("You don't have enough funds to cover the cost");
+		}
+		else
+		{
+			this.balance = this.balance-cost;
+		}
 		//Should return the remaining balance if they had enough money
-		return 0;
+		return this.balance;
 	}
 	
 }
